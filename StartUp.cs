0f35c313444d6464acc698c93a8d7a845ecce163
Jovan Popovic <jocapc@gmail.com>
2017-01-21 16:41:43 +0100

    Added startup method

--- StartUp.cs
+++ StartUp.cs
@@ -1,6 +1,7 @@
 ﻿using Belgrade.SqlClient;
 using Belgrade.SqlClient.SqlDb;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Data.SqlClient;
 
 namespace SqlServerRestApi
@@ -18,5 +19,56 @@ namespace SqlServerRestApi
 
             return services;
         }
+
+        public static IServiceCollection AddSqlClient(this IServiceCollection services, Action<Option> init)
+        {
+            Option options = new Option();
+            init(options);
+
+            // Adding data access services/components.
+            string QueryConnString = options.ConnString;
+            if(options.UseReadOnlyConnectionForQuery)
+                QueryConnString = options.ReadOnlyConnString;
+            string ConnString = options.ConnString;
+
+            switch (options.ServiceScope) {
+                case Option.ServiceScopeEnum.SCOPED:
+                    services.AddScoped<IQueryPipe>(
+                       sp => new QueryPipe(new SqlConnection(QueryConnString)));
+                    services.AddScoped<IQueryMapper>(
+                        sp => new QueryMapper(new SqlConnection(QueryConnString)));
+                    services.AddScoped<ICommand>(
+                        sp => new Command(new SqlConnection(ConnString)));
+                    break;
+                case Option.ServiceScopeEnum.TRANSIENT:
+                    services.AddTransient<IQueryPipe>(
+                       sp => new QueryPipe(new SqlConnection(QueryConnString)));
+                    services.AddTransient<IQueryMapper>(
+                        sp => new QueryMapper(new SqlConnection(QueryConnString)));
+                    services.AddTransient<ICommand>(
+                        sp => new Command(new SqlConnection(ConnString)));
+                    break;
+                case Option.ServiceScopeEnum.SINGLETON:
+                    services.AddSingleton<IQueryPipe>(
+                       sp => new QueryPipe(new SqlConnection(QueryConnString)));
+                    services.AddSingleton<IQueryMapper>(
+                        sp => new QueryMapper(new SqlConnection(QueryConnString)));
+                    services.AddSingleton<ICommand>(
+                        sp => new Command(new SqlConnection(ConnString)));
+                    break;
+            }
+            return services;
+        }
+    }
+
+    public class Option {
+
+        public string ConnString;
+        public bool UseReadOnlyConnectionForQuery = false;
+        public string ReadOnlyConnString;
+
+        public enum ServiceScopeEnum { SINGLETON, SCOPED, TRANSIENT };
+
+        public ServiceScopeEnum ServiceScope = ServiceScopeEnum.SCOPED;
     }
 }
