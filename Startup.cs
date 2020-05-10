7202a223b5c2e6bbd913331dc394188df1d5d4a1
Jovan Popovic <jocapc@gmail.com>
2020-05-11 00:10:57 +0200

    Changed GetConnectionString

--- Startup.cs
+++ Startup.cs
@@ -24,8 +24,8 @@ namespace BelgradeODataWebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddBelgradeSqlClient(Configuration["ConnectionStrings:Database"]);
+        {            
+            services.AddBelgradeSqlClient(Configuration.GetConnectionString("Database"));
             services.AddControllers();
         }
 
