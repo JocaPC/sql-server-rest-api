780c7347d93405577077fb5e1a1560831e512f02
Jovan Popovic <jocapc@gmail.com>
2020-05-10 18:58:39 +0200

    Fully functional demo of OData service

--- RestApi/OData/Aggregate.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MsSql.OData
-{
-    public class Aggregate
-    {
-        public string AggregateMethod = null;
-        public string AggregateColumn = null;
-        public string AggregateColumnAlias = null;
-    }
-}
--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,7 +25,7 @@ namespace DapperODataWebApi
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDapperSqlConnection(Configuration["ConnectionStrings:WWI"]);
+            services.AddDapperSqlConnection(Configuration["ConnectionStrings:Database"]);
             services.AddControllers();
         }
 
