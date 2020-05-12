1b32f22a8ddfe28469a8f3c97fd543bc481529ba
Jovan Popovic <jocapc@gmail.com>
2020-05-13 00:07:53 +0200

    New version

--- Startup.cs
+++ Startup.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using MsSql.RestApi;
+using TSql.RestApi;
 
 namespace BelgradeODataWebApi
 {
